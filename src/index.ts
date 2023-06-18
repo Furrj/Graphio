277c854cf34b83e6a7030bbacab532166c528271
Jackson Furr <jackson.a.furr@gmail.com>
2023-06-18 00:58:08 -0400

    init Draggable class

--- src/index.ts
+++ src/index.ts
@@ -2,18 +2,19 @@ import "./app";
 import "./css/index.css";
 import Renderer from "./utils/Render";
 import Component from "./utils/Component";
+import DraggableComponent from "./utils/Draggable";
 
 // COMPS
 import Vertex from "./comps/Vertex";
 
 // INIT
-const FPS: number = 1000;
+const FPS: number = 60;
 const renderer: Renderer = new Renderer();
-const vertex: Component = new Component(Vertex("one"));
+const vertex: DraggableComponent = new DraggableComponent(Vertex("one"));
 
 // MAIN LOOP
 const gameLoop: NodeJS.Timer = setInterval(() => {
-  vertex.changePosition(5, 0);
+  vertex.changePosition(1, 0);
 
   renderer.Render(vertex);
-}, FPS);
+}, Math.floor(1000 / FPS));
---
+++ src/utils/Draggable.ts
@@ -0,0 +1,9 @@
+import Component from "./Component";
+
+class DraggableComponent extends Component {
+  constructor(element: HTMLElement) {
+    super(element);
+  }
+}
+
+export default DraggableComponent;
