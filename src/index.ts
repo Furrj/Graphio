31e1a7378660548b1de2a4be6aa52420a58c734e
Jackson Furr <jackson.a.furr@gmail.com>
2023-06-18 00:50:02 -0400

    implemented basic game loop

--- src/index.ts
+++ src/index.ts
@@ -9,10 +9,10 @@ import Vertex from "./comps/Vertex";
 // INIT
 const FPS: number = 1000;
 const renderer: Renderer = new Renderer();
-const vertex: Component = new Component(Vertex("One"));
+const vertex: Component = new Component(Vertex("one"));
 
 // MAIN LOOP
-const gameLoop = setInterval(() => {
+const gameLoop: NodeJS.Timer = setInterval(() => {
   vertex.changePosition(5, 0);
 
   renderer.Render(vertex);
