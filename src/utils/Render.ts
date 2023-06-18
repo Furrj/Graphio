5357140047dc5ec14ef31886908f964038889eb3
Jackson Furr <jackson.a.furr@gmail.com>
2023-06-18 00:43:37 -0400

    added Component wrapper class

--- src/utils/Render.ts
+++ src/utils/Render.ts
@@ -1,3 +1,5 @@
+import Component from "./Component";
+
 class Renderer {
   root: Element | null;
 
@@ -5,9 +7,9 @@ class Renderer {
     this.root = document.querySelector("#root");
   }
 
-  Render(...elements: HTMLElement[]): void {
+  Render(...elements: Component[]): void {
     for (const element of elements) {
-      this.root?.appendChild(element);
+      this.root?.appendChild(element.getComponent());
     }
   }
 }
