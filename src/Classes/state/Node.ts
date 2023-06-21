110986ae15229487bc7e4b8ca4d39d41b6127dc6
Jackson Furr <jackson.a.furr@gmail.com>
2023-06-21 01:10:42 -0400

    implemented Node class

--- src/Classes/state/Node.ts
+++ src/Classes/state/Node.ts
@@ -1,5 +1,33 @@
 import Component from "../comps/Component";
 
-class Node {}
+class Node {
+  private component: Component;
+  private parent: Node | null = null;
+  private children: Node[] | null = null;
+
+  constructor(component: Component) {
+    this.component = component;
+  }
+
+  public getComponent(): Component {
+    return this.component;
+  }
+
+  public getParent(): Node | null {
+    return this.parent;
+  }
+
+  public setParent(parent: Node): void {
+    this.parent = parent;
+  }
+
+  public getChildren(): Node[] | null {
+    return this.children;
+  }
+
+  public addChild(child: Node): void {
+    this.children?.push(child);
+  }
+}
 
 export default Node;
