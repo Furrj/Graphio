d7d31dff2dab5d58912416b04f1dd598376520cf
Jackson Furr <jackson.a.furr@gmail.com>
2023-06-21 01:58:58 -0400

    button spawns vertexes

--- src/comps/Button.ts
+++ src/comps/Button.ts
@@ -1,8 +1,8 @@
-import "./css/app.css";
-
-const Button = (id: string): HTMLButtonElement => {
+const Button = (id: string, addVertex: () => void): HTMLButtonElement => {
   const spawnBtn: HTMLButtonElement = document.createElement("button");
   spawnBtn.id = id;
+  spawnBtn.className = "btn";
+  spawnBtn.addEventListener("click", addVertex);
   return spawnBtn;
 };
 
--- src/index.ts
+++ src/index.ts
@@ -6,13 +6,24 @@ import DraggableComponent from "./classes/comps/DraggableComponent";
 
 // COMPS
 import Vertex from "./comps/Vertex";
+import Button from "./comps/Button";
 
-// INIT
+// INIT FUNC
 const renderer: Renderer = new Renderer();
 
+// INIT COMPS
 const app: Component = new Component(App("app"));
 const vertex: DraggableComponent = new DraggableComponent(Vertex("one"), app);
+const spawnButton: Component = new Component(Button("spawnBtn", addVertex));
 
+function addVertex(): void {
+  const vertex: DraggableComponent = new DraggableComponent(Vertex("one"), app);
+  app.addChild(vertex);
+  renderer.render(app);
+}
+
+// LINKING
 app.addChild(vertex);
+app.addChild(spawnButton);
 
 renderer.render(app);
