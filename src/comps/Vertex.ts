2495500090f7ee657d06cfac8fd11d9d1631beb4
Jackson Furr <jackson.a.furr@gmail.com>
2023-06-23 01:38:41 -0400

    top and bottom node position

--- src/comps/Vertex.ts
+++ src/comps/Vertex.ts
@@ -1,11 +1,17 @@
 import Component from "../classes/comps/Component";
 import DraggableComponent from "../classes/comps/DraggableComponent";
-import State from "../state/State";
+import Node from "./Node";
 
 class Vertex extends DraggableComponent {
   constructor(parent: Component) {
     super(document.createElement("div"), parent);
     this.getElement().className = "vertex";
+    const nodeTop = new Node();
+    const nodeBottom = new Node();
+    nodeTop.getElement().classList.add("top");
+    nodeBottom.getElement().classList.add("bottom");
+    this.getElement().appendChild(nodeTop.getElement());
+    this.getElement().appendChild(nodeBottom.getElement());
   }
 }
 
--- src/index.ts
+++ src/index.ts
@@ -5,16 +5,12 @@ import init from "./utils/init";
 
 // COMPS
 import SpawnButton from "./comps/SpawnButton";
-import Node from "./comps/Node";
 
 // INIT FUNC
 const { state, renderer, app } = init();
 
 // INIT COMPS
 const spawnBtn: SpawnButton = new SpawnButton(state);
-const node: Node = new Node();
-
-app.addChild(node);
 
 // RENDERING
 renderer.render();
